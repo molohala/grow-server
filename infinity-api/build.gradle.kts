d1537a5d338b9bd78a8f68f98ace599b9bef86c0
김동찬 <129833370+dongchandev@users.noreply.github.com>
2024-04-11 09:05:35 +0900

    feat: swqgger 추가

--- infinity-api/build.gradle.kts
+++ infinity-api/build.gradle.kts
@@ -5,6 +5,11 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation ("org.springframework:spring-tx")
 
+    implementation ("io.springfox:springfox-swagger2:3.0.0")
+    implementation ("io.springfox:springfox-swagger-ui:3.0.0")
+    implementation ("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
+    implementation ("org.springframework.boot:spring-boot-starter-validation")
+
     implementation(project(":infinity-common"))
     implementation(project(":infinity-core"))
     implementation(project(":infinity-infra"))
