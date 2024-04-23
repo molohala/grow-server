3094281197a0106f8ab497634398410c2ae0a165
jombi <jombi@duck.com>
2024-04-23 09:25:03 +0900

    fix: set server timezone to KST

--- infinity-api/src/main/kotlin/com/molohala/InfinityApiApplication.kt
+++ infinity-api/src/main/kotlin/com/molohala/InfinityApiApplication.kt
@@ -1,10 +1,18 @@
 package com.molohala
 
+import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.TimeZone
 
 @SpringBootApplication
-class InfinityApiApplication
+class InfinityApiApplication {
+    @PostConstruct
+    fun init() {
+        // Timezone Fix
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<InfinityApiApplication>(*args)
