67eaeed6aea03856268604547691476346d19f2b
jombi <jombi@duck.com>
2024-04-24 11:45:12 +0900

    chore: add jedis to fix ClassNotFoundException

--- infinity-core/build.gradle.kts
+++ infinity-core/build.gradle.kts
@@ -1,22 +1,23 @@
 dependencies {
-    implementation ("org.springframework.boot:spring-boot-starter-web")
-    implementation ("org.springframework.boot:spring-boot-starter-validation")
-    kapt ("org.springframework.boot:spring-boot-configuration-processor")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    kapt("org.springframework.boot:spring-boot-configuration-processor")
 
-    implementation ("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("com.mysql:mysql-connector-j")
 
-    implementation ("org.springframework.boot:spring-boot-starter-data-redis")
-    runtimeOnly ("com.mysql:mysql-connector-j")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("redis.clients:jedis:5.1.2")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 
-    implementation ("com.querydsl:querydsl-jpa:5.0.0:jakarta")
-    implementation ("com.querydsl:querydsl-apt:5.0.0:jakarta")
+    implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+    implementation("com.querydsl:querydsl-apt:5.0.0:jakarta")
 
-    annotationProcessor ("com.querydsl:querydsl-apt:5.0.0:jakarta")
-    annotationProcessor ("jakarta.persistence:jakarta.persistence-api")
-    annotationProcessor ("jakarta.annotation:jakarta.annotation-api")
+    annotationProcessor("com.querydsl:querydsl-apt:5.0.0:jakarta")
+    annotationProcessor("jakarta.persistence:jakarta.persistence-api")
+    annotationProcessor("jakarta.annotation:jakarta.annotation-api")
 
     implementation(project(":infinity-common"))
 }
