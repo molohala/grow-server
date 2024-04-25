7e19e6ef34111abddcee76821162801af0c70c20
jombi <jombi@duck.com>
2024-04-25 19:42:46 +0900

    fix: redis config fix

--- infinity-core/src/main/kotlin/com/molohala/infinitycore/common/RedisConfig.kt
+++ infinity-core/src/main/kotlin/com/molohala/infinitycore/common/RedisConfig.kt
@@ -5,7 +5,6 @@ import com.molohala.infinitycore.rank.domain.dto.RedisSocialAccount
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
@@ -20,11 +19,8 @@ class RedisConfig(
     private val objectMapper: ObjectMapper,
 ) {
     @Bean
-    fun redisConnectionFactory(): RedisConnectionFactory = JedisConnectionFactory()
-
-    @Bean
-    fun redisTemplate(): RedisTemplate<String, RedisSocialAccount> {
-        return createTemplate(redisConnectionFactory(), Jackson2JsonRedisSerializer(objectMapper, RedisSocialAccount::class.java))
+    fun redisTemplate(connectionFactory: RedisConnectionFactory): RedisTemplate<String, RedisSocialAccount> {
+        return createTemplate(connectionFactory, Jackson2JsonRedisSerializer(objectMapper, RedisSocialAccount::class.java))
 //        return StringRedisTemplate().apply { connectionFactory = redisConnectionFactory() }
     }
 
