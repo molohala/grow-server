fa1b56ba4bc4793e5813fe7cd7540df49dc8a0f1
jombi <jombi@duck.com>
2024-05-01 20:03:53 +0900

    chore: naming change - send Query to back / reformat code

--- infinity-core/src/main/kotlin/com/molohala/infinitycore/community/application/service/CommunityService.kt
+++ infinity-core/src/main/kotlin/com/molohala/infinitycore/community/application/service/CommunityService.kt
@@ -12,7 +12,7 @@ import com.molohala.infinitycore.community.domain.consts.CommunityState
 import com.molohala.infinitycore.community.domain.entity.Community
 import com.molohala.infinitycore.community.domain.exception.CommunityNotFoundException
 import com.molohala.infinitycore.community.repository.CommunityJpaRepository
-import com.molohala.infinitycore.community.repository.QueryCommunityRepository
+import com.molohala.infinitycore.community.repository.CommunityQueryRepository
 import com.molohala.infinitycore.like.repository.LikeQueryRepository
 import com.molohala.infinitycore.member.application.MemberSessionHolder
 import com.molohala.infinitycore.member.domain.entity.Member
@@ -25,14 +25,14 @@ import org.springframework.transaction.annotation.Transactional
 @Transactional(readOnly = true)
 class CommunityService(
     private val communityJpaRepository: CommunityJpaRepository,
-    private val queryCommunityRepository: QueryCommunityRepository,
+    private val queryCommunityRepository: CommunityQueryRepository,
     private val queryCommentRepository: QueryCommentRepository,
     private val likeQueryRepository: LikeQueryRepository,
-    private val memberSessionHolder: MemberSessionHolder
+    private val memberSessionHolder: MemberSessionHolder,
 ) {
 
     @Transactional(rollbackFor = [Exception::class])
-    fun save(communitySaveReq: CommunitySaveReq){
+    fun save(communitySaveReq: CommunitySaveReq) {
         communityJpaRepository.save(
             Community(
                 communitySaveReq.content,
@@ -51,7 +51,7 @@ class CommunityService(
                     it.content,
                     it.createdAt,
                     likeQueryRepository.getCntByCommunityId(it.communityId),
-                    likeQueryRepository.existsByCommunityIdAndMemberId(it.communityId,it.writerId),
+                    likeQueryRepository.existsByCommunityIdAndMemberId(it.communityId, it.writerId),
                     it.writerName,
                     it.writerId
                 ), queryCommentRepository.findRecentComment(it.communityId)
@@ -63,16 +63,16 @@ class CommunityService(
         val member: Member = memberSessionHolder.current()
         val likeCnt: Long = likeQueryRepository.getCntByCommunityId(id)
         val isLike: Boolean = likeQueryRepository
-            .existsByCommunityIdAndMemberId(id,member.id!!)
+            .existsByCommunityIdAndMemberId(id, member.id!!)
         return queryCommunityRepository.findById(id, likeCnt, isLike)
     }
 
     @Transactional(rollbackFor = [Exception::class])
     fun modify(communityModifyReq: CommunityModifyReq) {
         val community = communityJpaRepository
-            .findByIdOrNull(communityModifyReq.id)?: throw CommunityNotFoundException()
+            .findByIdOrNull(communityModifyReq.id) ?: throw CommunityNotFoundException()
         val curMember = memberSessionHolder.current()
-        if(curMember.id != community.memberId){
+        if (curMember.id != community.memberId) {
             throw AccessDeniedException()
         }
         community.modify(communityModifyReq.content)
@@ -81,9 +81,9 @@ class CommunityService(
     @Transactional(rollbackFor = [Exception::class])
     fun delete(id: Long) {
         val community = communityJpaRepository
-            .findByIdOrNull(id)?: throw CommunityNotFoundException()
+            .findByIdOrNull(id) ?: throw CommunityNotFoundException()
         val curMember = memberSessionHolder.current()
-        if(curMember.id != community.memberId){
+        if (curMember.id != community.memberId) {
             throw AccessDeniedException()
         }
         community.delete()
