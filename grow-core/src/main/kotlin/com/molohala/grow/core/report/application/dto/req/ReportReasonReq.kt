72269d7cb4f72857c57576f14cba8371c73c0915
jombi <jombi@duck.com>
2024-05-23 15:14:23 +0900

    fix: json parsing error on one arg data class

--- grow-core/src/main/kotlin/com/molohala/grow/core/report/application/dto/req/ReportReasonReq.kt
+++ grow-core/src/main/kotlin/com/molohala/grow/core/report/application/dto/req/ReportReasonReq.kt
@@ -1,5 +1,7 @@
 package com.molohala.grow.core.report.application.dto.req
 
-data class ReportReasonReq(
+import com.fasterxml.jackson.annotation.JsonCreator
+
+data class ReportReasonReq @JsonCreator constructor(
     val reason: String
 )
