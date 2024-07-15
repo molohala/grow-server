702c29c3edf60ab4736c92cc1e0238601395ca5a
jombi <jombi@duck.com>
2024-07-16 00:18:25 +0900

    chore :: simplified library implementation using 'api' keyword

--- grow-core/build.gradle.kts
+++ grow-core/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    api("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     kapt("org.springframework.boot:spring-boot-configuration-processor")
 
@@ -9,8 +9,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("redis.clients:jedis:5.1.2")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 
     implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
     implementation("com.querydsl:querydsl-apt:5.0.0:jakarta")
@@ -19,8 +19,8 @@ dependencies {
     annotationProcessor("jakarta.persistence:jakarta.persistence-api")
     annotationProcessor("jakarta.annotation:jakarta.annotation-api")
 
-    implementation("com.google.firebase:firebase-admin:9.3.0")
-    implementation("io.netty:netty-codec-http:4.1.111.Final") // fix CVE-2024-29025 Vulnerability
+    api("com.google.firebase:firebase-admin:9.3.0")
+    api("io.netty:netty-codec-http:4.1.111.Final") // fix CVE-2024-29025 Vulnerability
 
     implementation(project(":grow-common"))
 }
--- grow-infra/build.gradle.kts
+++ grow-infra/build.gradle.kts
@@ -1,13 +1,9 @@
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     testImplementation("org.springframework.security:spring-security-test")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-
 
     implementation("org.springframework.boot:spring-boot-starter-graphql")
 
