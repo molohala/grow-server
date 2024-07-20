b97db6b23b9c219cd9348fd4bc56c5734af53d21
jombi <jombi@duck.com>
2024-07-20 22:55:10 +0900

    fix :: table naming

--- grow-core/src/main/kotlin/com/molohala/grow/core/notification/domain/entity/FCMToken.kt
+++ grow-core/src/main/kotlin/com/molohala/grow/core/notification/domain/entity/FCMToken.kt
@@ -4,7 +4,7 @@ import com.molohala.grow.core.common.BaseIdAndTimeEntity
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 
-@Entity
+@Entity(name = "tbl_fcm_tokens")
 data class FCMToken(
     @Column(nullable = false)
     val fcmToken: String,
